1916583a19c757b703f790048cdb1475a3c6e77f
Max Trapido <maxtrapido@gmail.com>
2023-12-24 18:24:04 -0700

    Finish ex 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  );
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.p1} {props.e1}
+      </p><p>
+        {props.p2} {props.e2}
+      </p><p>
+        {props.p3} {props.e3}
+      </p>
+    </div>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+    </div>
+  );
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header text={course} />
+      <Content p1={part1} e1={exercises1} p2={part2} e2={exercises2} p3={part3} e3={exercises3} />
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+    </div>
   )
 }
 
