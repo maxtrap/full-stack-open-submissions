44469e2c09f82ec90f41856da0d00ac07dd57881
Max Trapido <maxtrapido@gmail.com>
2023-12-24 17:46:46 -0700

    Add javascript expressions as props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,23 @@
-const Hello = (props) => (
-  <div>
-    <p>Hello {props.name}</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>Hello {props.name}, you weigh {props.weight} pounds</p>
+    </div>
+  );
+}
 
-const App = () => (
-  <div>
-    <h1>Greetings human!</h1>
-    <Hello name='George' />
-    <Hello name='Yellow hat man' />
-  </div>
-)
+const App = () => {
+  const name = 'Patrick Mahomes';
+  const weight = 225;
+
+  return (
+    <div>
+      <h1>Greetings human!</h1>
+      <Hello name={name} weight={weight} />
+      <Hello name='Lizzo' weight={203023+32423424}  />
+    </div>
+  )
+}
 
 export default App
