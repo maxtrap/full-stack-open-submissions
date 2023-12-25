1d0d4972283b7c6ab90af07855455975cb837da5
Max Trapido <maxtrapido@gmail.com>
2023-12-24 17:56:01 -0700

    Render some object in React

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,14 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>Hello {props.name}, you weigh {props.weight} pounds</p>
-    </div>
-  );
-}
-
 const App = () => {
-  const name = 'Patrick Mahomes';
-  const weight = 225;
+  const fighters = [
+    {name: 'Pat', age: '30'},
+    {name: 'Lizzo', age: '35'},
+  ]
 
   return (
-    <div>
-      <h1>Greetings human!</h1>
-      <Hello name={name} weight={weight} />
-      <Hello name='Lizzo' weight={203023+32423424}  />
-    </div>
+    <>
+      <p>{fighters[0].name} {fighters[0].age}</p>
+      <p>{fighters[1].name} {fighters[1].age}</p>
+    </>
   )
 }
 
