0246f7fea565b98884882810e38efe2a6521be35
Max Trapido <maxtrapido@gmail.com>
2023-12-24 19:47:56 -0700

    Finish ex 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,23 +7,23 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.p1} ex={props.e1} />
-      <Part part={props.p2} ex={props.e2} />
-      <Part part={props.p3} ex={props.e3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 }
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.ex}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
     </div>
   );
 }
@@ -32,24 +32,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header text={course} />
-      <Content p1={part1.name} e1={part1.exercises} p2={part2.name} e2={part2.exercises} p3={part3.name} e3={part3.name} />
-      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
