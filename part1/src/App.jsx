97470295fbff866c53b25a90e87aa7c0be5dbcc6
Max Trapido <maxtrapido@gmail.com>
2023-12-26 15:14:10 -0700

    Finish ex 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,59 +1,32 @@
-const Header = (props) => {
-  return (
-    <h1>{props.name}</h1>
-  );
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  );
-}
+const Header = ({ text }) => <h1>{text}</h1>;
 
-const Part = (props) => {
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-}
+const Button = ({ onClick, text }) =><button onClick={onClick}>{text}</button>;
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
-    </div>
-  );
-}
-
+const DisplayFeedback = ({ text, numRatings }) => <p>{text}: {numRatings}</p>
+  
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header text="Give your feedback:" />
+
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      <Header text="Statistics:" />
+
+      <DisplayFeedback text="good" numRatings={good} />
+      <DisplayFeedback text="neutral" numRatings={neutral} />
+      <DisplayFeedback text="bad" numRatings={bad} />
     </div>
   )
 }
