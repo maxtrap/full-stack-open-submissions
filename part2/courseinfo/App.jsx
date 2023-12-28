21c8286b003a06c8ac41be22faa3d71bceaac03a
Max Trapido <maxtrapido@gmail.com>
2023-12-28 14:59:32 -0700

    Finish ex 2.1

---
+++ part2/courseinfo/App.jsx
@@ -0,0 +1,67 @@
+const Header = (props) => {
+    return (
+      <h1>{props.name}</h1>
+    );
+  }
+  
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map(part => 
+          <Part key={part.id} part={part} />
+        )}
+      </div>
+    );
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>{props.part.name} {props.part.exercises}</p>
+    )
+  }
+  
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
+      </div>
+    );
+  }
+  
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+      </div>
+    );
+  };
+  
+  
+  const App = () => {
+    const course = {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return <Course course={course} />
+  }
+  
+  export default App
